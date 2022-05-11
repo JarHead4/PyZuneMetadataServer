85b33f8cdf2bf433cbd67b8f80b7fd207a8b5e61
JarHead <42592815+JarHead4@users.noreply.github.com>
2022-05-11 14:42:12 -0400

    Initial Upload

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, abort, Response, redirect
+import musicbrainzngs
+from musicbrainzngs.musicbrainz import ResponseError
+
+from atom.factory import *
+
+from locale import getdefaultlocale
+sys_locale = getdefaultlocale()[0]
+
+
+app = Flask(__name__)
+musicbrainzngs.set_useragent("Zune", "4.8")
+
+
+import re
+@app.after_request
+def allow_zunestk_cors(response):
+    return response
+
+def to_mb_toc(toc: str) -> str:
+    parts = toc.split('+')
+    count: int = len(parts)
+    
+    for i in range(count):
+        part: str = parts[i]
+
+        # Convert from hex to decimal
+        parts[i] = str(int(part, 16))
+
+    # Move section offset to position two
+    parts.insert(1, parts.pop(count - 1))
+
+    # Add first track
+    parts.insert(0, "1")
+
+    return '+'.join(parts)
+
+@app.route(f"/cdinfo/GetMDRCD.aspx")
+def cd_get_album():
+    # Get CD header from Zune request
+    ToC = request.args.get('CD')
+    # Replace spaces with +, so the MusicBrainz ToC script works
+    ToC = ToC.replace(" ", "+")
+    print("User inserted a CD with the ToC: " + ToC)
+    # Convert Zune ToC to MusicBrainz ToC
+    MBToC = to_mb_toc(ToC)
+    print("Converted ToC to MusicBrainz ToC: " + MBToC)
+    # Get album info from MusicBrainz
+    album = musicbrainzngs.get_releases_by_discid(MBToC, toc=MBToC, includes=["artists", "recordings"])
+    
+    album_name: str = album["release-list"][0]["title"]
+    artist: str = album['release-list'][0]['artist-credit'][0]['artist']['name']
+    # Check if MusicBrainz has a release date, if not set release_date to Unknown
+    try:
+        release_date: str = album['release-list'][0]['date']
+    except:
+        release_date: str = "Unknown"
+    # Zune will discard the entire XML if the release date is invalid, so we only send it if it's complete
+    if len(release_date) != 10:
+        release_date: str = "Unknown"
+    
+    numoftracks: str = album['release-list'][0]['medium-list'][0]['track-count']
+    MBID: str = album["release-list"][0]["id"]
+
+    print("Identified CD as: " + album_name + " by " + artist)
+    # Start building XML with new data
+    xml = "<METADATA><MDR-CD><version>5.0</version><WMCollectionID>" + MBID + "</WMCollectionID><WMCollectionGroupID>" + MBID + "</WMCollectionGroupID><uniqueFileID>UMGa_id=R   123480</uniqueFileID><albumTitle>" + album_name + "</albumTitle><albumArtist>" + artist + "</albumArtist>" 
+    # Remove the release date if it's incomplete or unavailable
+    if release_date != "Unknown":
+        xml = xml + "<releaseDate>" + release_date + "</releaseDate>"
+    xml = xml + "<label>UMG</label><genre>Pop</genre><providerStyle>Pop/Rock</providerStyle><publisherRating>5</publisherRating><buyParams>providerName=UMG&amp;albumID=" + MBID + "&amp;a_id=R%20%20%20123480&amp;album=Go%20West%20Young%20Man&amp;artistID=D82033BF-D711-4442-94D6-1196E76223F4&amp;p_id=P%20%20%20%20%202400&amp;artist=Michael%20W.%20Smith</buyParams><largeCoverParams>/large/album.jpg?id=" + MBID + "</largeCoverParams><smallCoverParams>/small/album.jpg?id=" + MBID + "</smallCoverParams><moreInfoParams>a_id=R%20%20%20123480</moreInfoParams><dataProvider>AMG</dataProvider><dataProviderParams>Provider=AMG</dataProviderParams><dataProviderLogo>Provider=AMG</dataProviderLogo><needIDs>0</needIDs>"
+    # Add track info to XML for each track
+    for i in range(numoftracks):
+        # There's probably a better way to do all this, but no
+        x = int("%d" % (i))
+        tracknum: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['number']
+        trackid: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['recording']['id']
+        tracktitle: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['recording']['title']
+        tracktitle = tracktitle.replace("&", "&amp;")
+        xml = xml + "<track><WMContentID>" + trackid + "</WMContentID><trackTitle>" + tracktitle + "</trackTitle><uniqueFileID>UMGp_id=P     2400;UMGt_id=T  2881042</uniqueFileID><trackNumber>" + tracknum + "</trackNumber><trackPerformer>" + artist + "</trackPerformer><trackComposer>" + artist + "</trackComposer><explicitLyrics>0</explicitLyrics></track>"
+    # Finalize XML
+    xml = xml + "</MDR-CD><Backoff><Time>5</Time></Backoff></METADATA>"
+    print("Converted " + album_name + " to XML")
+    return Response(xml, mimetype=MIME_XML)
+    print("Sent XML to client")
+
+@app.route(f"/cdinfo/GetMDRCDPOSTURL.aspx")
+def get_post_url():
+    with open(f'GetMDRCDPOSTURL.aspx', 'r') as file:
+        data: str = file.read().replace('\n', '')
+        return Response(data, mimetype=MIME_ATOM_XML)
+
+# Get large image information
+@app.route(f"/cover/large/album.jpg")
+def cd_get_large():
+    # Get id header from Zune request
+    AlbumId = request.args.get('id')
+
+    image = musicbrainzngs.get_image_front(AlbumId, size=1200)
+
+    return Response(image, mimetype=MIME_JPG)
+
+# Get small image information
+@app.route(f"/cover/small/album.jpg")
+def cd_get_small():
+    # Get id header from Zune request
+    AlbumId = request.args.get('id')
+
+    image = musicbrainzngs.get_image_front(AlbumId, size=500)
+
+    return Response(image, mimetype=MIME_JPG)
+
+if __name__ == "__main__":
+    app.run(port=80, host="127.0.0.3")
---
+++ atom/factory.py
@@ -0,0 +1,98 @@
+from typing import Dict, Any
+from xml.dom import minidom
+from xml.dom.minidom import Element, Document, Text
+from datetime import datetime
+
+MIME_XML = "text/xml"
+MIME_APP_XML = "application/xml"
+MIME_ATOM_XML = "application/atom+xml"
+MIME_UIX = "application/uix"
+MIME_JPG = "image/jpeg"
+
+
+def set_element_value(element: Element, value: str):
+    content = Text()
+    content.data = value
+    element.appendChild(content)
+
+
+def set_value_as_element(doc: Document, element: Element, name: str, value: Any):
+    prop_element: Element = doc.createElement(name)
+    if type(value) is dict:
+        set_values_as_elements(doc, prop_element, value)
+    else:
+        set_element_value(prop_element, value)
+    element.appendChild(prop_element)
+
+
+def set_values_as_elements(doc: Document, element: Element, props: Dict[str, Any]):
+    for name in props:
+        set_value_as_element(doc, element, name, props[name])
+
+
+def create_feed(doc: Document, title: str, id: str, href: str, date_updated: datetime = datetime.today()) -> Element:
+    feed = create_empty_feed(doc)
+
+    feed.appendChild(create_link(doc, href))
+    feed.appendChild(create_updated(doc, date_updated))
+    feed.appendChild(create_title(doc, title))
+    feed.appendChild(create_id(doc, id))
+
+    return feed
+
+
+def create_empty_feed(doc: Document) -> Element:
+    # Add namespaces
+    feed: Element = doc.createElement("a:feed")
+    feed.setAttribute("xmlns:a", "http://www.w3.org/2005/Atom")
+    feed.setAttribute("xmlns:os", "http://a9.com/-/spec/opensearch/1.1/")
+    feed.setAttribute("xmlns", "http://schemas.zune.net/catalog/music/2007/10")
+    doc.appendChild(feed)
+
+    return feed
+
+
+def create_link(doc: Document, href: str, rel: str = "self", type: str = MIME_ATOM_XML) -> Element:
+    link: Element = doc.createElement("a:link")
+    link.setAttribute("rel", rel)
+    link.setAttribute("type", type)
+    link.setAttribute("href", href)
+    return link
+
+
+def create_updated(doc: Document, date_updated: datetime = datetime.today()) -> Element:
+    updated: Element = doc.createElement("a:updated")
+    set_element_value(updated, date_updated.isoformat())
+    return updated
+
+
+def create_title(doc: Document, title: str, type: str = "text") -> Element:
+    title_elem: Element = doc.createElement("a:title")
+    title_elem.setAttribute("type", type)
+    set_element_value(title_elem, title)
+    return title_elem
+
+
+def create_id(doc: Document, id: str) -> Element:
+    id_elem: Element = doc.createElement("a:id")
+    set_element_value(id_elem, id)
+    return id_elem
+
+
+def create_entry(doc: Document, title: str, id: str, href: str, date_updated: datetime = datetime.today()) -> Element:
+    entry: Element = doc.createElement("a:entry")
+
+    entry.appendChild(create_link(doc, href))
+    entry.appendChild(create_updated(doc, date_updated))
+    entry.appendChild(create_title(doc, title))
+    entry.appendChild(create_id(doc, id))
+
+    return entry
+
+
+def create_author(doc: Document, name: str) -> Element:
+    author_elem: Element = doc.createElement("a:author")
+    author_name_elem: Element = doc.createElement("a:name")
+    set_element_value(author_name_elem, name)
+    author_elem.appendChild(author_name_elem)
+    return author_elem
