b719a78da00b149992d9bae95887bfbf372ed43b
JarHead <42592815+JarHead4@users.noreply.github.com>
2022-05-11 23:11:40 -0400

    Add region detection for albums
    The XML will now use data for your region, instead of the first album in the list.

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, abort, Response, redirect
 import musicbrainzngs
 from musicbrainzngs.musicbrainz import ResponseError
+import utils
 
 from atom.factory import *
 
 from locale import getdefaultlocale
 sys_locale = getdefaultlocale()[0]
 
-
 app = Flask(__name__)
 musicbrainzngs.set_useragent("Zune", "4.8")
-
 
 import re
 @app.after_request
@@ -39,6 +38,10 @@ def to_mb_toc(toc: str) -> str:
 def cd_get_album():
     # Get CD header from Zune request
     ToC = request.args.get('CD')
+    # Get geoId header from Zune request
+    geoid = request.args.get('geoid')
+    # Get country abbreviation from geoid
+    country = utils.get_country(geoid)
     # Replace spaces with +, so the MusicBrainz ToC script works
     ToC = ToC.replace(" ", "+")
     print("User inserted a CD with the ToC: " + ToC)
@@ -47,22 +50,25 @@ def cd_get_album():
     print("Converted ToC to MusicBrainz ToC: " + MBToC)
     # Get album info from MusicBrainz
     album = musicbrainzngs.get_releases_by_discid(MBToC, toc=MBToC, includes=["artists", "recordings"])
-    
-    album_name: str = album["release-list"][0]["title"]
+    print(album)
+
+    releasenum = utils.get_release_by_country(country, album)
+    print(releasenum)
+    album_name: str = album["release-list"][releasenum]["title"]
     album_name = album_name.replace("&", "&amp;")
-    artist: str = album['release-list'][0]['artist-credit'][0]['artist']['name']
+    artist: str = album['release-list'][releasenum]['artist-credit'][0]['artist']['name']
     artist = artist.replace("&", "&amp;")
     # Check if MusicBrainz has a release date, if not set release_date to Unknown
     try:
-        release_date: str = album['release-list'][0]['date']
+        release_date: str = album['release-list'][releasenum]['date']
     except:
         release_date: str = "Unknown"
     # Zune will discard the entire XML if the release date is invalid, so we only send it if it's complete
     if len(release_date) != 10:
         release_date: str = "Unknown"
     
-    numoftracks: str = album['release-list'][0]['medium-list'][0]['track-count']
-    MBID: str = album["release-list"][0]["id"]
+    numoftracks: str = album['release-list'][releasenum]['medium-list'][0]['track-count']
+    MBID: str = album["release-list"][releasenum]["id"]
 
     print("Identified CD as: " + album_name + " by " + artist)
     # Start building XML with new data
@@ -75,13 +81,20 @@ def cd_get_album():
     for i in range(numoftracks):
         # There's probably a better way to do all this, but no
         x = int("%d" % (i))
-        tracknum: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['number']
-        trackid: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['recording']['id']
-        tracktitle: str = album['release-list'][0]['medium-list'][0]['track-list'][x]['recording']['title']
+        tracknum: str = album['release-list'][releasenum]['medium-list'][0]['track-list'][x]['number']
+        try:
+            trackid: str = album['release-list'][releasenum]['medium-list'][0]['track-list'][x]['id']
+        except:
+            trackid: str = album['release-list'][releasenum]['medium-list'][0]['track-list'][x]['recording']['id']
+        try:
+            tracktitle: str = album['release-list'][releasenum]['medium-list'][0]['track-list'][x]['title']
+        except:
+            tracktitle: str = album['release-list'][releasenum]['medium-list'][0]['track-list'][x]['recording']['title']
         tracktitle = tracktitle.replace("&", "&amp;")
         xml = xml + "<track><WMContentID>" + trackid + "</WMContentID><ZuneMediaID>" + trackid + "</ZuneMediaID><trackTitle>" + tracktitle + "</trackTitle><uniqueFileID>UMGp_id=P     2400;UMGt_id=T  2881042</uniqueFileID><trackNumber>" + tracknum + "</trackNumber><trackPerformer>" + artist + "</trackPerformer><trackComposer>" + artist + "</trackComposer><explicitLyrics>0</explicitLyrics></track>"
     # Finalize XML
     xml = xml + "</MDR-CD><Backoff><Time>5</Time></Backoff></METADATA>"
+    print(xml)
     print("Converted " + album_name + " to XML")
     return Response(xml, mimetype=MIME_XML)
     print("Sent XML to client")
--- utils.py
+++ utils.py
@@ -1,13 +1,45 @@
-import urllib.request
-import urllib.error
-import json
-from typing import Union
 
-
-def get_json(url: str) -> Union[int, dict]:
+def get_country(geoid):
+    match geoid:
+        case "f4":
+            return "US"
+        case "292d":
+            return "XE"
+        case "7c":
+            return "JP"
+        case "27":
+            return "CA"
+        case "f2":
+            return "GB"
+        case "54":
+            return "FR"
+        case "b0":
+            return "NL"
+        case "dd":
+            return "SE"
+        case "xe":
+            return "AT"
+        case "5e":
+            return "DE"
+        case "989e":
+            return "XW"
+        case "68":
+            return "HK"
+        case "a7":
+            return "MY"
+        case "d7":
+            return "SG"
+        case "xc":
+            return "AU"
+            
+def get_release_by_country(country, album):
+    releases = int(album['release-count'])-int("1")
     try:
-        response = urllib.request.urlopen(url)
-    except urllib.error.HTTPError as error:
-        return error.code
-
-    return json.load(response)
+        for i in range(releases):
+            x = int("%d" % (i))
+            if album["release-list"][x]["country"] == country:
+                return x
+            elif x == releases:
+                return 0
+    except:
+        return 0
